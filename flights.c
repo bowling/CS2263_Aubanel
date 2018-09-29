33a26f5f145774b23f452a61e5e1d03edf9e45ee
Michael Redbourne <mredbourne12@gmail.com>
2018-09-29 15:46:56 -0300

    Add files via upload

--- flights.c
+++ flights.c
@@ -36,6 +36,8 @@ void findClosestFlight(int desiredTime, int *departureTime, int *arrivalTime){
   int num1, num2, num3, num4;
   FILE * fp = fopen("flights.dat","r");
   while((fscanf(fp, "%d:%d", &num1, &num2) == 2) && (fscanf(fp, "%d:%d", &num3, &num4) == 2)){
+	printf("\nTime 1: %d:%d", num1, num2);
+	printf("\nTime 2: %d:%d", num3, num4);
 	*departureTime = (num1*60) + num2;
 	*arrivalTime = (num3*60) + num4;
 	
