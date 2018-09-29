17a2e027b3d5c0ed4badfa4ff549d48da72eb574
Michael Redbourne <mredbourne12@gmail.com>
2018-09-29 16:27:11 -0300

    Add files via upload

--- flights.c
+++ flights.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 void findClosestFlight(int desiredTime, int *departureTime, int *arrivalTime);
 
@@ -25,19 +24,19 @@ int main(){
   num3 = arrivalTime / 60;
 
   if(num1 < 10){
-   	char[3] num01 = "0" + num1 + '\0';
+   	char[3] num5 = "0" + num1 + '\0';
   }
   if(num2 < 10){
-  	char[3] num02 = "0" + num2 + '\0';
+  	char[3] num6 = "0" + num2 + '\0';
   }
   if(num3 < 10){
-  	char[3] num03 = "0" + num3 + '\0';
+  	char[3] num7 = "0" + num3 + '\0';
   }
   if(num4 < 10){
-  	char[3] num04 = "0" + num4 + '\0';
+  	char[3] num8 = "0" + num4 + '\0';
   }
-  char[12] numberFormatting1 = num01 + ":" + num02;
-  char[12] numberFormatting2 = num03 + ":" + num04;
+  char[12] numberFormatting1 = num5 + ":" + num6;
+  char[12] numberFormatting2 = num7 + ":" + num8;
 
   printf("The closest time to your desired time departs @ %s and arrives at %s", numberFormatting1, numberFormatting2);
 }
